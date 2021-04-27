b72f2f6d03b14f3f2e5e07ddf84ab60c3d5d284e
LegItMate <77503777+LegItMate@users.noreply.github.com>
2021-04-27 23:12:26 +0530

    Some error handling and fixed mistakes

--- src/commands/misc/say.js
+++ src/commands/misc/say.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args, utils) => {
 	const saymessage = args.join(' ');
 	if(!saymessage) return message.channel.send('Please provide something for me to say!');
 	message.delete().catch(err => console.log(err));
-	message.channel.send(Discord.Util.removeMentions('**' + saymessage + '**\n\n -' + message.author.username));
+	message.channel.send('**' + Discord.Util.escapeMarkdown(saymessage) + '**\n\n_ _   - ' + message.author.username, { allowedMentions: { parse: [], users: [], roles: [] } });
 };
 
 
--- src/commands/soundboard/lessgoo.js
+++ src/commands/soundboard/lessgoo.js
@@ -3,13 +3,15 @@ const Discord = require('discord.js');
 const path = require('path');
 const checkifalreadyplaying = new Discord.Collection();
 module.exports.run = async (client, message, args, utils, data) => {
+	if (!message.guild.me.hasPermission(['CONNECT', 'SPEAK'])) return message.reply('I do not have permissions to join voice channels!', { allowedMentions: { repliedUser: false } });
 	const channel = message.member.voice.channel;
-	if(!channel) return message.channel.send('please connect to a voice channel to use soundboard');
+	if (!channel) return message.channel.send('please connect to a voice channel to use soundboard');
+	if (!channel.joineable || !channel.speakable) return message.reply('I do not have permissions to join that voice channel!', { allowedMentions: { repliedUser: false } });
 	channel.join().then(connection => {
 		const dispatcher = connection.play(path.join(__dirname + '/audio/lessgoo.mp3'));
-		message.react('🔊');
+		message.react('🔊').catch(err => undefined);
 		dispatcher.on('speaking', speaking => {
-			if(!speaking) channel.leave();
+			if (!speaking) channel.leave();
 		});
 	}).catch(err => console.log(err));
 };
