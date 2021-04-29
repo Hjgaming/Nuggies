b85d9278fd6d2268d36237c16fae8973812671c2
LegItMate <77503777+LegItMate@users.noreply.github.com>
2021-04-29 16:23:49 +0530

    Different queue(doesn't work like queue but handles errors)

--- src/commands/soundboard/fart.js
+++ src/commands/soundboard/fart.js
@@ -1,29 +1,15 @@
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 const path = require('path');
-const checkifalreadyplaying = new Discord.Collection();
+const play = require('../../../functions/playstream');
 module.exports.run = async (client, message, args, utils, data) => {
 	if (!message.guild.me.hasPermission(['CONNECT', 'SPEAK'])) return message.reply('I do not have permissions to join voice channels!', { allowedMentions: { repliedUser: false } });
 	const channel = message.member.voice.channel;
 	if (!channel) return message.channel.send('please connect to a voice channel to use soundboard');
 	if (!channel.joinable || !channel.speakable) return message.reply('I do not have permissions to join that voice channel!', { allowedMentions: { repliedUser: false } });
-	if (!client.soundboardqueue.get(message.guild.id)) client.soundboardqueue.set(message.guild.id, []);
-	let connection;
-	if (!message.guild.me.voice.channel) connection = await channel.join();
-	else connection = message.guild.me.voice.connection;
 
-	const dispatcher = connection.play(path.join(__dirname + '/audio/fart.mp3'));
+	play(message, path.join(__dirname + '/audio/fart.mp3'));
 	message.react('🔊').catch(err => undefined);
-	client.soundboardqueue.get(message.guild.id).push(client.soundboardqueue.length + 1);
-	dispatcher.on('speaking', speaking => {
-		if (!speaking) {
-			client.soundboardqueue.get(message.guild.id).shift();
-			if (client.soundboardqueue.get(message.guild.id).length == 0) {
-				channel.leave();
-				client.soundboardqueue.delete(message.guild.id);
-			}
-		}
-	});
 };
 
 module.exports.help = {
--- src/commands/soundboard/tts.js
+++ src/commands/soundboard/tts.js
@@ -1,34 +1,19 @@
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
-const path = require('path');
 const config = require('../../../utils/config.json');
 const discordTTS = require('discord-tts');
-const checkifalreadyplaying = new Discord.Collection();
+const play = require('../../../functions/playstream');
 module.exports.run = async (client, message, args, utils, data) => {
 	if (!message.guild.me.hasPermission(['CONNECT', 'SPEAK'])) return message.reply('I do not have permissions to join voice channels!', { allowedMentions: { repliedUser: false } });
 	const channel = message.member.voice.channel;
 	if (!args) return message.channel.send('Please provide something to convert to TTS!');
 	if (!channel) return message.channel.send('please connect to a voice channel to use TTS.');
 	if (!channel.joinable || !channel.speakable) return message.reply('I do not have permissions to join that voice channel!', { allowedMentions: { repliedUser: false } });
-	if (!client.soundboardqueue.get(message.guild.id)) client.soundboardqueue.set(message.guild.id, []);
-	let connection;
-	if (!message.guild.me.voice.channel) connection = await channel.join();
-	else connection = message.guild.me.voice.connection;
 
 	const broadcast = client.voice.createBroadcast();
 	broadcast.play(discordTTS.getVoiceStream(`${args.join(' ')}`));
-	const dispatcher = connection.play(broadcast);
+	play(message, broadcast);
 	message.react('🔊').catch(err => undefined);
-	client.soundboardqueue.get(message.guild.id).push(client.soundboardqueue.length + 1);
-	dispatcher.on('speaking', speaking => {
-		if (!speaking) {
-			client.soundboardqueue.get(message.guild.id).shift();
-			if (client.soundboardqueue.get(message.guild.id).length == 0) {
-				channel.leave();
-				client.soundboardqueue.delete(message.guild.id);
-			}
-		}
-	});
 };
 
 module.exports.help = {
