00824f8380d2c90ca956fa788e9b162408113316
IYR3775 <66664082+IYR3775@users.noreply.github.com>
2021-05-02 21:06:27 +0500

    Update eval.js

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -6,9 +6,14 @@ function clean(text) {
 
 module.exports.run = async (client, message, args, utils, data) => {
 	const code = args.join(' ');
+        const hasAwait = code.includes("await"),
+        const hasReturn = code.includes("return"),
 	try {
-		let evaled = eval(`(async () => { ${code} })()`);
-		if (typeof evaled !== "string") evaled = require("util").inspect(evaled, { depth: 0 });
+		let evaled = hasAwait ? await eval(`(async () => { ${hasReturn ? " " : "return"} ${code} })()`) : eval(code);
+		if (typeof evaled !== "string") { 
+evaled = require("util").inspect(evaled, { depth: Number(message.content.slice(-1)) || +!(require("util").inspect(evaled, { depth: 2 }))
+});
+		}
 		message.channel.send(`\`\`\`xl\n${clean(evaled)}\n\`\`\``);
 	}
 	catch (err) {
