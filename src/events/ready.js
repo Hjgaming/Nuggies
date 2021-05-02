db9838de0be290372869d75f42eceb709b8947c0
LegItMate <77503777+LegItMate@users.noreply.github.com>
2021-05-02 19:15:40 +0530

    Reverting ready event changes

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,11 +4,10 @@ const Discord = require('discord.js');
 const automeme = require('../../models/guilds');
 const config = require('../../utils/config.json');
 const axios = require('axios');
-const Cache = require('../../functions/cache');
+
 module.exports = async (client) => {
 	console.log(`${client.user.username} is now online!`);
 	client.user.setActivity('bot.nuggetdev.com/premium', { type: 'WATCHING', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
-
 	if (client.user.id !== '779741162465525790') return;
 	const Webhook = new Discord.WebhookClient(process.env.ready_webhook_id, process.env.ready_webhook_token);
 	Webhook.send(new Discord.MessageEmbed().setTitle('Nuggies was restarted!').setDescription('Nuggies just got restarted!').setColor('e03854').setTimestamp());
