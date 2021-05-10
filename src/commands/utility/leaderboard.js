57763e8e6cd0df123cd267661cdf8b5589ddcd44
Assassin#1234 <chordsandfrets@gmail.com>
2021-05-11 01:12:54 +0530

    Update leaderboard.js

--- src/commands/utility/leaderboard.js
+++ src/commands/utility/leaderboard.js
@@ -5,24 +5,28 @@ module.exports.run = async (client, message, args, utils) => {
 	const lb = await client.data.pointsleaderboard(10);
 
 	await lb.leaderboard.forEach(async (i) => {
-	  try {
-		await client.users.fetch(i.id);
-	  } catch (error) {
-		return;
-	  }
+		try {
+			await client.users.fetch(i.id);
+		}
+		catch (error) {
+			return;
+		}
 	});
-	index = 1; //${index += 1}.
-	mapped = await lb.leaderboard.map(
-	  (i) =>
-		`\`${index += 1}\`- **${i.points.toLocaleString()}** - ${
-		  client.users.cache.get(i.id) ? client.users.cache.get(i.id).tag : "Unknown#0000"
-		}`
+	let index = 1; // ${index += 1}.
+	const mapped = await lb.leaderboard.map(
+		(i) =>
+			`\`${index += 1}\`- **${i.points.toLocaleString()}** - ${
+				client.users.cache.get(i.id) ? client.users.cache.get(i.id).tag : 'Unknown#0000'
+			}`,
 	);
 	return message.channel.send(
 		new Discord.MessageEmbed()
-		.setTitle('Global Points Leaderboard')
-		.setDescription(`${mapped.join("\n")}`)
-		.setTimestamp()
+			.addField('What are the points for? \n ', 'You get points for afk-ing on https://afk.nuggetdev.com/ \n _ _')
+			.addField('what to do with these points? \n', 'the top 3 people with the most points at the end of the month will get amazing prizes like **Discord Nitro** :smirk: \n _ _')
+			.addField('Points:', `${mapped.join('\n')}`)
+			.setTitle('Global Points Leaderboard')
+			.setTimestamp()
+			.setColor('RANDOM'),
 	);
 };
 
