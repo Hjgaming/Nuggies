e72fae5a01f683e54a3e571a53f2313a93c31b69
LegItMate <77503777+LegItMate@users.noreply.github.com>
2021-05-15 15:14:46 +0530

    Schema

---
+++ models/giveaways.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+	guildID: {
+		type: String, required: true,
+	},
+	channelID: {
+		type: String, required: true,
+	},
+	messageID: {
+		type: String, required: true,
+	},
+	hoster: {
+		type: String, required: true,
+	},
+	time: {
+		type: Date, required: true,
+	},
+	prize: {
+		type: String, required: true,
+	},
+	winners: {
+		type: Number, required: true,
+	},
+});
+
+module.exports = mongoose.model('giveaways', schema);
